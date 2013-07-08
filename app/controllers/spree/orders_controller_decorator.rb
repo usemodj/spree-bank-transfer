8bad54bf34922ee30e3f74dccce909470af7fe53
Seo Seokjin <usemodj@gmail.com>
2013-07-08 11:50:30 +0900

    modified:   app/controllers/spree/orders_controller_decorator.rb

--- app/controllers/spree/orders_controller_decorator.rb
+++ app/controllers/spree/orders_controller_decorator.rb
@@ -3,10 +3,10 @@ module Spree
     def confirm_payment
       @order = Order.find_by_number!(params[:id])
       
-      twilio_sid = @order.payment.payment_method.preferred_twilio_sid
-      twilio_token = @order.payment.payment_method.preferred_twilio_token
-      twilio_from = @order.payment.payment_method.preferred_twilio_from
-      twilio_to = @order.payment.payment_method.preferred_twilio_to
+      twilio_sid = @order.payments.last.payment_method.preferred_twilio_sid
+      twilio_token = @order.payments.last.payment_method.preferred_twilio_token
+      twilio_from = @order.payments.last.payment_method.preferred_twilio_from
+      twilio_to = @order.payments.last.payment_method.preferred_twilio_to
 
       sms_body = "#{params[:name]} has transferred #{Spree::Config.currency} " +
       "#{params[:amount]} to #{params[:bank_name]} for order #{@order.number}"
