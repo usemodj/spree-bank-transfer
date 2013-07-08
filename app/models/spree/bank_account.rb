f6d0a9e1601006e776e42823cd300112a809d544
Seo Seokjin <usemodj@gmail.com>
2013-07-08 16:05:26 +0900

    modified:   app/models/spree/bank_account.rb

--- app/models/spree/bank_account.rb
+++ app/models/spree/bank_account.rb
@@ -1,7 +1,9 @@
 module Spree
 	class BankAccount < ActiveRecord::Base
-		attr_accessible :bank_name, :bank_branch, :account_owner, :account_number
+		attr_accessible :payment_method_id, :bank_name, :bank_branch, :account_owner, :account_number
 		validates :bank_name, :bank_branch, :account_owner, :account_number, :presence => true
 		validates :account_number, :numericality => { :only_integer => true }
+
+		belongs_to :payment_method
 	end
 end
